3ecd2b98e787796d7aaa2e368144a9cf6ee21a7e
Pavel Tsiukhtsiayeu <paveltyk@gmail.com>
2019-03-29 17:01:53 +0300

    EctoAdapter pt.2

--- lib/epgsql_ex/ecto_adapter/connection.ex
+++ lib/epgsql_ex/ecto_adapter/connection.ex
@@ -17,8 +17,64 @@ defmodule EpgsqlEx.EctoAdapter.Connection do
   end
 
   @impl true
-  def all(_query) do
-    :TODO
+  def all(query) do
+    sources = create_names(query)
+    from = from(query, sources)
+    select = select(query, sources)
+
+    [select, from]
+  end
+
+  # Query generation
+
+  defp select(%{select: %{fields: fields}}, sources) do
+    fields = intersperse_map(fields, ", ", &expr(&1, sources), [])
+    ["SELECT ", fields]
+  end
+
+  defp from(%{from: %{source: source}}, sources) do
+    {from, name} = get_source(sources, 0, source)
+    [" FROM ", from, " AS ", name]
+  end
+
+  defp expr({{:., _, [{:&, _, [idx]}, field]}, _, []}, sources) do
+    {_, source, _} = elem(sources, idx)
+    [source, ?., quote_name(field)]
+  end
+
+  defp create_names(%{sources: sources}) do
+    create_names(sources, 0, tuple_size(sources)) |> List.to_tuple()
+  end
+
+  defp create_names(sources, pos, limit) when pos < limit do
+    [create_name(sources, pos), create_names(sources, pos + 1, limit)]
+  end
+
+  defp create_names(_sources, pos, pos), do: []
+
+  defp create_name(sources, pos) do
+    {table, schema, _prefix} = elem(sources, pos)
+    name = [create_alias(table), Integer.to_string(pos)]
+    {quote_name(table), name, schema}
+  end
+
+  defp create_alias(<<first, _rest::binary>>) when first in ?a..?z when first in ?A..?Z do
+    <<first>>
+  end
+
+  defp get_source(sources, ix, source) do
+    {expr, name, _schema} = elem(sources, ix)
+    {expr || expr(source, sources), name}
+  end
+
+  defp quote_name(name) when is_atom(name), do: quote_name(Atom.to_string(name))
+
+  defp quote_name(name), do: [?", name, ?"]
+
+  defp intersperse_map([elem], _separator, mapper, acc), do: [acc, mapper.(elem)]
+
+  defp intersperse_map([elem | rest], separator, mapper, acc) do
+    intersperse_map(rest, separator, mapper, [acc, mapper.(elem), separator])
   end
 
   # Not implemented
--- lib/epgsql_ex/query.ex
+++ lib/epgsql_ex/query.ex
@@ -8,7 +8,29 @@ defmodule EpgsqlEx.Query do
 
     def encode(_query, params, _opts), do: params
 
-    def decode(_query, result, _opts), do: result
+    def decode(_query, {:ok, columns, rows}, _opts) do
+      rows = Enum.map(rows, &normalize_row(&1, columns))
+      %{columns: columns, rows: rows, num_rows: length(rows)}
+    end
+
+    defp normalize_row(row, columns) when is_tuple(row) and is_list(columns) do
+      row |> Tuple.to_list() |> normalize_row(columns, [])
+    end
+
+    defp normalize_row([val | vals], [col | cols], acc) do
+      type = elem(col, 2)
+
+      casted_val =
+        case {type, val} do
+          {:int4, val} when is_binary(val) -> String.to_integer(val)
+          {:int8, val} when is_binary(val) -> String.to_integer(val)
+          {_type, val} -> val
+        end
+
+      normalize_row(vals, cols, acc ++ [casted_val])
+    end
+
+    defp normalize_row([], [], acc), do: acc
   end
 
   defimpl String.Chars do
